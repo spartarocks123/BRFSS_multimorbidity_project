86a6680f6e6984ae4242976919bd71083ef7fbbe
spartarocks123 <spartarocks123@gmail.com>
2026-04-15 19:00:47 -0500

    Auto-commit from Raycast

---
+++ scripts/01_load_data.R
@@ -0,0 +1,18 @@
+library(haven)
+library(dplyr)
+library(survey)
+library(ggplot2)
+library(viridis)
+library(scales)
+library(broom)
+library(tidyverse)
+library(ggeffects)
+library(pROC)
+
+# ------------------------------
+# Load Data
+# ------------------------------
+brfss <- read_xpt("/filepath/LLCP2024.XPT ")
+
+# Clean names (removes leading underscores)
+names(brfss) <- gsub("^_", "", names(brfss))
